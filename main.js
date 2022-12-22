7d84d39c1a8129a52acd859d2be15bfe82161db0
thesourorange <thesoururange@outlook.com>
2022-12-23 09:40:07 +1100

    First Commit

---
+++ main.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const config = require('./config.json');
+
+const {app, protocol, BrowserWindow} = require('electron');
+
+const path = require('path');
+
+var mainWindow = null;
+app.allowRendererProcessReuse = true;
+
+function createWindow () {
+
+    mainWindow = new BrowserWindow({width: config.mode == 'debug' ? 1200: 800, height: 560, 
+        resizable: false, autoHideMenuBar: true,  
+        webPreferences: {
+        }
+    });
+
+    if (config.mode == "debug") {
+        mainWindow.webContents.openDevTools();
+    }
+
+    mainWindow.setMenu(null);
+    mainWindow.loadURL(`file:///${path.join(__dirname, 'index.html')}`);
+
+    mainWindow.on('closed', () => {
+        mainWindow = null
+    })
+
+}
+ 
+app.on('ready', createWindow)
+
+app.on('window-all-closed',  () => {
+    app.quit()
+})
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow()
+    }
+
+});
